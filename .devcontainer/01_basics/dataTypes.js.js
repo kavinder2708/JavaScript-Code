c6cabbdcf8682b4f391ebf5cb195195f85ed5aef
Kavinder Choudhary <kavinder@etelligens.in>
2026-05-26 11:38:44 +0000

    dataTypes

---
+++ .devcontainer/01_basics/dataTypes.js.js
@@ -0,0 +1,12 @@
+// We have 7 different data types inside primitive data types i.e.
+// number ==> 24
+// strings  ==>""
+// booleans  ==> true/false
+// bigInt.  ==>. number> 2 power of 56
+// symbols  ==> unique 
+// undefined 
+// null
+
+
+console.log('typeof  undefiles', undefined); // undefine
+console.log('typeof null ', null);   // object
